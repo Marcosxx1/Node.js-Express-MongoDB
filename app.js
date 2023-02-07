8dab99d059d7abe33c1985beca1c60a7dcf31c19
Marcos Alex <marcosxx1.ma@gmail.com>
2023-02-06 20:20:52 -0400

    Refatoração do script

--- app.js
+++ app.js
@@ -7,37 +7,15 @@ var app = express();
 //de solicitação reebidos, fica entre a solicitação e resposta
 app.use(express.json());
 var tours = JSON.parse(fs.readFileSync("".concat(__dirname, "/dev-data/data/tours-simple.json")));
-//adiciona os dados
-/* req = tem os dados necessários */
-app.post("/api/v1/tours", function (req, res) {
-    // console.log(req.body);
-    var newId = tours[tours.length - 1].id + 1; //Calculando a nova ID para o novo POST
-    var newTour = Object.assign({ id: newId }, req.body); //copiando a estrutura de tours para newTour
-    tours.push(newTour); //Colocando newTour no array de tours, linha 11
-    //usando o módulo fs, e o módulo writeFile para escrever no arquivo 'tours.json', JSON.stringify(tours) Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
-    fs.writeFile("".concat(__dirname, "/dev-data/data/tours-simple.json"), JSON.stringify(tours), function (_err) {
-        //201 = foi ecrito com sucesso
-        res.status(201).json({
-            status: "success",
-            //valor a ser passado
-            data: {
-                tour: newTour
-            }
-        });
-    });
-});
-//implementação do get, pega os dados
-app.get("/api/v1/tours", function (req, res) {
+//função para o get
+var getAllTours = function (req, res) {
     res.status(200).json({
         results: tours.length,
         status: "success",
         data: { tours: tours }
     });
-});
-//um get para pegar pela id depois do :
-//podemos deixar parametros incertos com ?  /:id/:nome?
-app.get('/api/v1/tours/:id', function (req, res) {
-    //  console.log(req.params);
+};
+var getTour = function (req, res) {
     //multiplicar uma string com um numero resulta em um numero
     var id = req.params.id * 1;
     //Implementação do vídeo
@@ -61,10 +39,29 @@ app.get('/api/v1/tours/:id', function (req, res) {
             tour2: tour2
         }
     });
-});
-/* PUT - é esperado que receba o objeto inteiro
-   PATCH - algumas propriedades */
-app.patch('/api/v1/tours/:id', function (req, res) {
+};
+//adiciona os dados
+/* req = tem os dados necessários */
+app.post("/api/v1/tours");
+//cria variável para segurar a função de criar tour
+var createTour = function (req, res) {
+    // console.log(req.body);
+    var newId = tours[tours.length - 1].id + 1; //Calculando a nova ID para o novo POST
+    var newTour = Object.assign({ id: newId }, req.body); //copiando a estrutura de tours para newTour
+    tours.push(newTour); //Colocando newTour no array de tours, linha 11
+    //usando o módulo fs, e o módulo writeFile para escrever no arquivo 'tours.json', JSON.stringify(tours) Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+    fs.writeFile("".concat(__dirname, "/dev-data/data/tours-simple.json"), JSON.stringify(tours), function (_err) {
+        //201 = foi ecrito com sucesso
+        res.status(201).json({
+            status: "success",
+            //valor a ser passado
+            data: {
+                tour: newTour
+            }
+        });
+    });
+};
+var updateTour = function (req, res) {
     if (req.params.id > tours.length) {
         return res.status(404).json({
             status: "fail",
@@ -77,7 +74,40 @@ app.patch('/api/v1/tours/:id', function (req, res) {
             tour: '<Updated toursss>'
         }
     });
-});
+};
+var deleteTour = function (req, res) {
+    if (req.params.id > 1 * tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: "ID not found"
+        });
+    }
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+};
+//implementação do get, pega os dados
+//app.get('/api/v1/tours', getAllTours);                ///////////////1
+//um get para pegar pela id depois do :
+//podemos deixar parametros incertos com ?  /:id/:nome?
+//app.get('/api/v1/tours/:id', getTour)                  ///////////////2
+//adiciona os dados
+/* req = tem os dados necessários */
+//app.post("/api/v1/tours", createTour);                 ///////////////3
+/* PUT - é esperado que receba o objeto inteiro
+   PATCH - algumas propriedades */
+//app.patch('/api/v1/tours/:id', updateTour)             ///////////////4
+//Deleta tour
+//app.delete('/api/v1/tours/:id',deleteTour)             ///////////////5
+/* Forma mais prática para definir as rotas
+sem repetir muito código */
+app.route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createTour);
+app.route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour)["delete"](deleteTour);
 app.listen(3000, function () {
     console.log("Servidor rodando na porta 3000");
 });
--- app.ts
+++ app.ts
@@ -12,78 +12,18 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-//adiciona os dados
-/* req = tem os dados necessários */
-app.post(
-    "/api/v1/tours",
-    (
-        req: { body: any },
-        res: {
-            status: (arg0: number) => {
-                (): any;
-                new(): any;
-                json: {
-                    (arg0: { status: string; data: { tour: {} } }): void;
-                    new(): any;
-                };
-            };
-            send: (arg0: string) => void;
-        }
-    ) => {
-        // console.log(req.body);
-
-        const newId = tours[tours.length - 1].id + 1; //Calculando a nova ID para o novo POST
-        const newTour = Object.assign({ id: newId }, req.body); //copiando a estrutura de tours para newTour
-
-        tours.push(newTour); //Colocando newTour no array de tours, linha 11
-
-        //usando o módulo fs, e o módulo writeFile para escrever no arquivo 'tours.json', JSON.stringify(tours) Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
-        fs.writeFile(
-            `${__dirname}/dev-data/data/tours-simple.json`,
-            JSON.stringify(tours),
-            (_err: any) => {
-                //201 = foi ecrito com sucesso
-                res.status(201).json({
-                    status: "success",
-                    //valor a ser passado
-                    data: {
-                        tour: newTour,
-                    },
-                });
-            }
-        );
-    }
-);
-
-//implementação do get, pega os dados
-app.get(
-    "/api/v1/tours",
-    (
-        req: any,
-        res: {
-            status: (arg0: number) => {
-                (): any;
-                new(): any;
-                json: {
-                    (arg0: { results: any; status: string; data: { tours: any } }): void;
-                    new(): any;
-                };
-            };
-        }
-    ) => {
-        res.status(200).json({
-            results: tours.length,
-            status: "success",
-            data: { tours },
-        });
-    }
-);
 
 
-//um get para pegar pela id depois do :
-//podemos deixar parametros incertos com ?  /:id/:nome?
-app.get('/api/v1/tours/:id', (req: any, res: any) => {
-  //  console.log(req.params);
+//função para o get
+const getAllTours = (req: any, res: any) => {
+    res.status(200).json({
+        results: tours.length,
+        status: "success",
+        data: { tours },
+    });
+}
+
+const getTour = (req: any, res: any) => {
 
     //multiplicar uma string com um numero resulta em um numero
     const id = req.params.id * 1;
@@ -113,28 +53,96 @@ app.get('/api/v1/tours/:id', (req: any, res: any) => {
             tour2
         }
     })
-})
+}
+//adiciona os dados
+/* req = tem os dados necessários */
+app.post("/api/v1/tours",);
+//cria variável para segurar a função de criar tour
+const createTour = (req: any, res: any) => {
+    // console.log(req.body);
 
-/* PUT - é esperado que receba o objeto inteiro
-   PATCH - algumas propriedades */
+    const newId = tours[tours.length - 1].id + 1; //Calculando a nova ID para o novo POST
+    const newTour = Object.assign({ id: newId }, req.body); //copiando a estrutura de tours para newTour
 
-   app.patch('/api/v1/tours/:id',(req: any, res: any) =>{
-    if(req.params.id > tours.length){
+    tours.push(newTour); //Colocando newTour no array de tours, linha 11
+
+    //usando o módulo fs, e o módulo writeFile para escrever no arquivo 'tours.json', JSON.stringify(tours) Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        (_err: any) => {
+            //201 = foi ecrito com sucesso
+            res.status(201).json({
+                status: "success",
+                //valor a ser passado
+                data: {
+                    tour: newTour,
+                },
+            });
+        }
+    );
+}
+
+const updateTour = (req: any, res: any) => {
+    if (req.params.id > tours.length) {
         return res.status(404).json({
             status: "fail",
             message: "ID not found"
         })
     }
 
- 
+
     res.status(200).json({
-        status:'success',
+        status: 'success',
         data: {
             tour: '<Updated toursss>'
         }
     })
-   })
+}
 
-    app.listen(3000, function () {
-        console.log("Servidor rodando na porta 3000");
-    });
+const deleteTour = (req: any, res: any) => {
+    if (req.params.id > 1 * tours.length) {
+        return res.status(404).json({
+            status: "fail",
+            message: "ID not found"
+        })
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+}
+
+//implementação do get, pega os dados
+//app.get('/api/v1/tours', getAllTours);                ///////////////1
+
+//um get para pegar pela id depois do :
+//podemos deixar parametros incertos com ?  /:id/:nome?
+//app.get('/api/v1/tours/:id', getTour)                  ///////////////2
+
+//adiciona os dados
+/* req = tem os dados necessários */
+//app.post("/api/v1/tours", createTour);                 ///////////////3
+
+
+/* PUT - é esperado que receba o objeto inteiro
+   PATCH - algumas propriedades */
+//app.patch('/api/v1/tours/:id', updateTour)             ///////////////4
+
+//Deleta tour
+//app.delete('/api/v1/tours/:id',deleteTour)             ///////////////5
+
+/* Forma mais prática para definir as rotas
+sem repetir muito código */
+app.route('/api/v1/tours')
+.get(getAllTours)
+.post(createTour)
+
+app.route('/api/v1/tours/:id')
+.get(getTour)
+.patch(updateTour)
+.delete(deleteTour)
+
+app.listen(3000, function () {
+    console.log("Servidor rodando na porta 3000");
+});
