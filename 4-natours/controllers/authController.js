13eaf6e88e68b4b0d3238d971e1e73675f57dec7
Marcos Alex <marcosxx1.ma@gmail.com>
2023-02-21 16:15:49 -0400

    Utilizando JWT para autenticação

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -1,14 +1,52 @@
+/* $ npm i jsonwebtoken utilizado para autenticação de usuário */
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
-    const a = 0;
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+    });
 
+    const token = signToken(newUser._id);
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser,
         },
+    });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // 1) Checar se o email e senha existem
+    if (!email || !password) {
+        next(new AppError('Please provide email and password', 400));
+    }
+
+    // 2) Checar se o usuário existe && senha está correta
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect email or passowrd', 401));
+    }
+
+    // 3) se tudo estiver certo, enviar o token para o cliente
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token,
     });
 });
