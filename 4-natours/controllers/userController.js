88782b245f7e4802b5131b138914cc26cb1114f5
Marcos Alex <marcosxx1.ma@gmail.com>
2023-02-23 17:53:15 -0400

    Atualizações rotas

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
     const users = await User.find();
@@ -9,6 +18,38 @@ exports.getAllUsers = catchAsync(async (req, res) => {
         results: users.length,
         data: {
             users,
+        },
+    });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    // 1) Criar erro se usuário POSTa dados de senha
+    if (req.body.password || req.body.confirmPassword) {
+        return next(
+            new AppError(
+                'This route is not for password updates, please use updateMyPassword',
+                400
+            )
+        );
+    }
+
+    // 2) Filtrando campos de nomes indesejados que não são permitidos para atualizar
+    const filteredBody = filterObj(req.body, 'name', 'email');
+
+    // 3) Atualizar documento
+    const updatedUser = await User.findByIdAndUpdate(
+        req.user.id,
+        filteredBody,
+        {
+            new: true,
+            runValidators: true,
+        }
+    );
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser,
         },
     });
 });
