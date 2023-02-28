c3780328d834748f6462aa0fd17f34f30a703851
Marcos Alex <marcosxx1.ma@gmail.com>
2023-02-28 18:44:17 -0400

    Implementação cookies

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -64,6 +64,37 @@ exports.login = catchAsync(async (req, res, next) => {
     createSendToken(user, 200, res);
 });
 
+//Apenas para páginas renderizadas, não haverá erros
+exports.isLoggedIn = async (req, res, next) => {
+    if (req.cookies.jwt) {
+      try {
+        // 1) verify token
+        const decoded = await promisify(jwt.verify)(
+          req.cookies.jwt,
+          process.env.JWT_SECRET
+        );
+  
+        // 2) Check if user still exists
+        const currentUser = await User.findById(decoded.id);
+        if (!currentUser) {
+          return next();
+        }
+  
+        // 3) Check if user changed password after the token was issued
+        if (currentUser.changedPasswordAfter(decoded.iat)) {
+          return next();
+        }
+  
+        // THERE IS A LOGGED IN USER
+        res.locals.user = currentUser;
+        return next();
+      } catch (err) {
+        return next();
+      }
+    }
+    return next();
+  };
+
 exports.protect = catchAsync(async (req, res, next) => {
     // 1) Adquirir o token e checar se ele existe
     let token;
@@ -233,4 +264,3 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
     // 7) Log the user in, send JWT
     createSendToken(user, 200, res);
 });
-  
--- 4-natours/public/js/login.js
+++ 4-natours/public/js/login.js
@@ -1,5 +1,7 @@
-const login = async (email, password) => {
-    console.log(email, password);
+import axios from 'axios';
+import { showAlert } from './alert';
+
+export const login = async (email, password) => {
     try {
         const res = await axios({
             method: 'POST',
@@ -9,16 +11,14 @@ const login = async (email, password) => {
                 password,
             },
         });
+        if (res.data.status === 'success') {
+            showAlert('success ', 'Logged in successfully');
+            window.setTimeout(() => {
+                location.assign('/');
+            }, 500);
+        }
         console.log(res);
     } catch (err) {
-        console.log(err.response.data);
+        showAlert('error', err.response.message);
     }
 };
-
-document.querySelector('.form').addEventListener('submit', e => {
-    e.preventDefault();
-
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    login(email, password);
-});
